d4cfd2ab72e519b12777604a6d70b0ead8c44767
Jugadu Developers <132820792+JugaduDevelopers@users.noreply.github.com>
2023-08-23 20:34:07 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -85,9 +85,7 @@ const Home = (props: Props) => {
           </h1>
           <div className="xl:absolute xl:top-36 xl:left-[15%] flex flex-col sm:flex-row items-center  justify-between">
             <div>
-              <h3 className="text-[18px] font-900 uppercase leading-[120%]">
-                <span className="text-white">SkinCare -</span> Assistt
-              </h3>
+              
               <h1 className=" text-[40px] md xl:text-[55px] font-900 leading-[120%]">
                 <span className="text-white">
                   {" "}
